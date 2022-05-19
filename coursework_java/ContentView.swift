046b3a7faca70ce62b7493e00eb91a56e137f4f2
Denis Beloshitskiy <beloshitskiy.denis@gmail.com>
2022-05-19 14:43:36 +0300

    Initial Commit

---
+++ coursework_java/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coursework_java
+//
+//  Created by Denis Beloshitskiy on 5/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coursework_java/coursework_javaApp.swift
@@ -0,0 +1,17 @@
+//
+//  coursework_javaApp.swift
+//  coursework_java
+//
+//  Created by Denis Beloshitskiy on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct coursework_javaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
